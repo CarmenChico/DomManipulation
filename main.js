233fd29c5e68121dc3cd3391cb56b4889246746f
Carmen Chico <CarmenChico@icloud.com>
2016-10-05 22:16:39 -0400

    tried code

---
+++ main.js
@@ -0,0 +1,16 @@
+ var resultsbox= document.getElementById("answer");
+
+
+    var value-one,value-two, answer;
+    function setValues()
+    {
+      value-one = number (document.getElementById(value-one).value);
+      value-two =number (document.getElementById(value-two).value);
+    }
+
+}
+function calculate-button() {
+    resultsbox.innerHTML = "";
+    answer = value-one + value-two;
+    alert ("the sum is equals to the"+ answer);
+}
