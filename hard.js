833f17c4c3339f17bfb60f340c66b99610a7db3e
Carmen Chico <CarmenChico@icloud.com>
2016-10-19 06:05:53 -0400

    adjusted specifics and tried out hard mode via guided exercies

---
+++ hard.js
@@ -0,0 +1,56 @@
+var calcState = {
+  left: "",
+  right: "",
+  operator: ""
+};
+
+var processButton = function (event) {
+  var target = event.target;
+  var buttonType = target.dataset.type;
+
+  if (buttonType === "operator") { processOperator(target); }
+  if (buttonType === "number")   { processNumber(target); }
+  if (buttonType === "clear")    { processClear(target); }
+  if (buttonType === "equal")    { processEqual(target); }
+
+  updateDisplay();
+};
+
+var updateDisplay = function () {
+  var display = document.getElementById("display");
+  display.textContent = calcState.left + " " + calcState.operator + " " + calcState.right;
+};
+
+var updateCalc = function (newLeft) {
+  calcState = { left: newLeft, right: "", operator: "" };
+};
+
+var processClear = function () {
+  calcState = { left: "", right: "", operator: "" };
+};
+
+var processOperator = function (element) {
+  calcState.operator = element.textContent;
+};
+
+var processNumber = function (element) {
+  if (calcState.operator === "") {
+    calcState.left += element.textContent;
+  } else {
+    calcState.right += element.textContent;
+  }
+};
+
+var processEqual = function (element) {
+  var operation = calcState.operator;
+  var left = Number(calcState.left);
+  var right = Number(calcState.right);
+
+  if (operation === "+") { updateCalc(left + right); }
+  if (operation === "-") { updateCalc(left - right); }
+  if (operation === "*") { updateCalc(left * right); }
+  if (operation === "/") { updateCalc(left / right); }
+};
+
+var controls = document.getElementById("controls");
+controls.addEventListener("click", processButton);
--- main.js
+++ main.js
@@ -1,20 +1,29 @@
+//identify the button
+var button = document.getElementById ("calculate-button");
 
-var sumImports = function () {
-    var input1= document.getElementById("value-one");
-    var input2= document.getElementById("value-two");
-    var sum = Number (input1.value) + Number (input2.value);
 
+//adds both boxes
+  function sumOfEntry () {
+    //pull the value from the id's
+    //identify each box entry
+    var firstEntry= document.getElementById("value-one");
+    var secondEntry= document.getElementById("value-two");
+    //create an addition of both entry boxes
+    var sum = Number (firstEntry.value) + Number (secondEntry.value);
+//the results the computer will give is the sum
     return sum;
 };
 
-
-var getAnswer = function (event) {
-    event.preventDefault ();
-    var answer = sumImports ();
-    var resultsbox= document.getElementById("answer");
-    resultsbox.textContent = answer;
+//this will place the answer we calculated into the section specified for the answer to appear on the screen
+  function answeredCalculation (event) {
+      event.preventDefault ();
+      //we pull the answer from the function (sumOfEntry) above
+      var answer = sumOfEntry ();
+      var resultsbox= document.getElementById("answer");
+//adds the information into the section of the answer//
+//I'm thinking we could also use innerHTML here (which we would include in addtion to the answer the word answer as it would be replaced when we call for it via innerHTML)
+      resultsbox.textContent = answer;
  };
 
-
-var button = document.getElementById ("calculate-button");
-    button.addEventListener("click", getAnswer);
+//makes the button preform the action of populating the answered calculation into the section previously itentified
+  button.addEventListener("click", answeredCalculation);
